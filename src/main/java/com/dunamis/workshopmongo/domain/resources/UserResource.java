b25df7d306f5da781a04eb2ec4339bf94992aa5c
danielvtfs <danielvtfs@gmail.com>
2022-06-01 07:20:18 -0300

    creating update user

--- src/main/java/com/dunamis/workshopmongo/domain/resources/UserResource.java
+++ src/main/java/com/dunamis/workshopmongo/domain/resources/UserResource.java
@@ -50,6 +50,15 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO userDto, @PathVariable String id){
+		User user = userService.fromDTO(userDto);
+		user.setId(id);
+		
+		user = userService.update(user);
+		return ResponseEntity.noContent().build();
+	}
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		userService.delete(id);
--- src/main/java/com/dunamis/workshopmongo/services/UserService.java
+++ src/main/java/com/dunamis/workshopmongo/services/UserService.java
@@ -11,34 +11,43 @@ import com.dunamis.workshopmongo.dto.UserDTO;
 import com.dunamis.workshopmongo.repositories.UserRepository;
 import com.dunamis.workshopmongo.services.exception.ObjectNotFoundException;
 
-
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
 
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User user) {
 		return userRepository.insert(user);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		userRepository.deleteById(id);
 	}
-	
-	public User fromDTO(UserDTO userDto) {
-		return new User(userDto.getId(), userDto.getName(),userDto.getEmail());
+
+	public User update(User obj) {
+		User newUser = findById(obj.getId());
+		updateData(newUser, obj);
+		return userRepository.save(newUser);
 	}
-	
-	
+
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
+	}
+
 }
